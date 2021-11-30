1097e33c54816d2fccdfba651b4e455c508184bf
kobiyoyo <adamachubi@gmail.com>
2021-11-30 23:24:44 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  devise_for :users,
+             controllers: {
+                 sessions: 'users/sessions',
+                 registrations: 'users/registrations'
+             }
+  get '/member-data', to: 'members#show'
+  resources :posts
+end
---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+# 10.times{
+#     Post.create(title: Faker::Company.catch_phrase, description: Faker::TvShows::TheFreshPrinceOfBelAir.quote , user_id: 1 )
+# }
+
+10.times{
+    Post.create(title: Faker::Company.catch_phrase, description: Faker::TvShows::TheFreshPrinceOfBelAir.quote , user_id: 2 )
+}
